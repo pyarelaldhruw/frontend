8ed1d5a8726ee40d739a0a960aec16f9769e878a
Pyarelal Dhruw <pyarelaldhruw750@gmail.com>
2024-11-13 01:46:49 +0530

    javascript

---
+++ javascript/programs/evenodd.js
@@ -0,0 +1,8 @@
+let no=11;
+if(no%2==0){
+    console.log(no," is even number")
+
+}else{
+    console.log(no," is odd number")
+
+}
---
+++ javascript/programs/printable.js
@@ -0,0 +1,4 @@
+let no=10;
+for(let i=1; i<=12; i++){
+    console.log(i*no);
+}
